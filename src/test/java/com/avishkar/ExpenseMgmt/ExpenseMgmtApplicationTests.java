d49299c39cd1c364741186742b6628c2e8c6aaf7
Avishkar794 <avishkarjoshi2@gmail.com>
2026-07-10 14:11:14 +0530

    Initial commit

---
+++ src/test/java/com/avishkar/ExpenseMgmt/ExpenseMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avishkar.ExpenseMgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
